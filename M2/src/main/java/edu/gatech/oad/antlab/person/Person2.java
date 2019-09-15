3d93467e1c282969faab8963569413c7ca314409
RobertGiuffreda <rgiuffreda3@gatech.edu>
2019-09-15 14:54:13 -0400

    First

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,5 +1,6 @@
 package edu.gatech.oad.antlab.person;
-
+import java.util.Collections;
+import java.util.ArrayList;
 /**
  *  A simple class for person 2
  *  returns their name and a
@@ -31,7 +32,16 @@ public class Person2 {
 	 */
 	private String calc(String input) {
 	  //Person 2 put your implementation here
-	  return null;
+		String shuffled = "";
+		ArrayList<String> list = new ArrayList<>(input.length());
+		for (int i = 0; i < input.length(); i++) {
+			list.add(input.charAt(i) + "");
+		}
+		Collections.shuffle(list);
+		for (String letter: list) {
+			shuffled += letter;
+		}
+		return shuffled;
 	}
 	/**
 	 * Return a string rep of this object
