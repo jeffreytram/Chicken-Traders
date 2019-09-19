98287b83d74465da3c4193604d010a66dbf82c08
jeffreytram <jeffrey_tram@yahoo.com>
2019-09-19 18:07:37 -0400

    Added difficulty and starting credits selection, Added button style and highlight.

--- Space Trader/static/script.js
+++ Space Trader/static/script.js
@@ -5,6 +5,7 @@ let settings = {
     fighterSP: 0,
     merchantSP: 0,
     engineerSP: 0,
+    credits: 0,
     saveName: function () {
         let nameTF = document.getElementById("name");
         this.name = nameTF.value;
@@ -19,11 +20,15 @@ let settings = {
         this.merchantSP = merchantTF.value;
         this.engineerSP = engineerTF.value;
     },
-    setName: function () {
+    saveDifficulty: function() {
+        let selectedDiff = document.getElementsByClassName("active");
+        this.difficulty = selectedDiff[0].textContent;
+    },
+    displayName: function () {
         let nameP = document.getElementById("displayName");
         nameP.innerHTML = this.name;
     },
-    setSP: function () {
+    displaySP: function () {
         let pilotP = document.getElementById("displayPilotSP");
         let fighterP = document.getElementById("displayFighterSP");
         let merchantP = document.getElementById("displayMerchantSP");
@@ -32,6 +37,26 @@ let settings = {
         fighterP.innerHTML = this.fighterSP;
         merchantP.innerHTML = this.merchantSP;
         engineerP.innerHTML = this.engineerSP;
+    },
+    displayDifficulty: function() {
+        let diffP = document.getElementById("difficulty");
+        diffP.innerHTML = this.difficulty;
+    },
+    setDifficulty: function(diff){
+        this.difficulty = diff;
+    },
+    setCredits: function(){
+        if(this.difficulty === "easy"){
+            this.credits = 1000;
+        } else if (this.difficulty === "medium"){
+            this.credits = 800;
+        } else {
+            this.credits = 500;
+        }
+    },
+    displayCredits: function(){
+        let creditsP = document.getElementById("credits");
+        creditsP.innerHTML = this.credits;
     }
 }
 let handlers = {
@@ -42,34 +67,66 @@ let handlers = {
         currHidden[0].className = currHidden[0].className.replace(" hidden", "");
         currShow[0].className += " hidden";
     },
-    saveSettings: function () {
+    setSettings: function () {
         //save name
         settings.saveName();
-        //TODO: save difficulty chose
         //save skill point allocation
         settings.saveSP();
+        //save difficulty
+        settings.saveDifficulty();
+        //set credits
+        settings.setCredits();
     },
     displaySelectedSettings: function () {
         //display name
-        settings.setName();
+        settings.displayName();
         //display SP
-        settings.setSP();
+        settings.displaySP();
+        //display difficulty
+        settings.displayDifficulty();
+        //display credits
+        settings.displayCredits();
+    },
+    setDifficulty: function(diff){
+        settings.setDifficulty(diff);
+    },
+    highlightButton: function(button){
+        let active = document.getElementsByClassName("active");
+        if (active.length > 0) {
+            active[0].className = active[0].className.replace(" active", "");
+        }
+        button.className += " active";
     }
 }
 let view = {
     setUpEventListeners: function () {
+        //WELCOME SECTION
         let buttonDiv = document.getElementById("buttonDiv");
         buttonDiv.addEventListener("click", function (event) {
             if (event.target.className === "start") {
                 handlers.displayAndHide("settings", "welcome");
             }
         });
+        //SETTINGS SECTION
         let continueButton = document.getElementById("continueButton");
         continueButton.addEventListener("click", function () {
             handlers.displayAndHide("confirm", "settings");
-            handlers.saveSettings();
+            handlers.setSettings();
             handlers.displaySelectedSettings();
         });
+        let diffButtonDiv = document.getElementById("difficultyDiv");
+        diffButtonDiv.addEventListener("click", function (event) {
+            if (event.target.id === "easy") {
+                handlers.highlightButton(event.target);
+            }
+            if (event.target.id === "medium") {
+                handlers.highlightButton(event.target);
+            }
+            if (event.target.id === "hard") {
+                handlers.highlightButton(event.target);
+            }
+        });
+        //CONFIRM SECTION
         let backButton = document.getElementById("backButton");
         backButton.addEventListener("click", function () {
             handlers.displayAndHide("settings", "confirm");
