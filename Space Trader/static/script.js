10c56767d43ad9f3ecee1e4f4b587499b3a25288
jeffreytram <jeffrey_tram@yahoo.com>
2019-09-19 16:30:19 -0400

    Added confirmation screen, Updated README.md

--- Space Trader/static/script.js
+++ Space Trader/static/script.js
@@ -1,26 +1,83 @@
+let settings = {
+    name: "",
+    difficulty: "",
+    pilotSP: 0,
+    fighterSP: 0,
+    merchantSP: 0,
+    engineerSP: 0,
+    saveName: function () {
+        let nameTF = document.getElementById("name");
+        this.name = nameTF.value;
+    },
+    saveSP: function () {
+        let pilotTF = document.getElementById("pilotSP");
+        let fighterTF = document.getElementById("fighterSP");
+        let merchantTF = document.getElementById("merchantSP");
+        let engineerTF = document.getElementById("engineerSP");
+        this.pilotSP = pilotTF.value;
+        this.fighterSP = fighterTF.value;
+        this.merchantSP = merchantTF.value;
+        this.engineerSP = engineerTF.value;
+    },
+    setName: function () {
+        let nameP = document.getElementById("displayName");
+        nameP.innerHTML = this.name;
+    },
+    setSP: function () {
+        let pilotP = document.getElementById("displayPilotSP");
+        let fighterP = document.getElementById("displayFighterSP");
+        let merchantP = document.getElementById("displayMerchantSP");
+        let engineerP = document.getElementById("displayEngineerSP");
+        pilotP.innerHTML = this.pilotSP;
+        fighterP.innerHTML = this.fighterSP;
+        merchantP.innerHTML = this.merchantSP;
+        engineerP.innerHTML = this.engineerSP;
+    }
+}
 let handlers = {
-    displayText: function () {
-        let hiddenElements = document.getElementsByClassName("hidden");
-        let welcomeElements = document.getElementsByClassName("welcome");
-        hiddenElements[0].className = hiddenElements[0].className.replace(" hidden", "");
-        welcomeElements[0].className += " hidden";
-        /*
-        hiddenElements.forEach(function (element, position) {
-            element.className = hiddenElements[position].className.replace(" hidden", "");
-        });
-        welcomeElements.forEach(function (element, position) {
-            element[position].className += " hidden";
-        });
-        */
+    displayAndHide: function (eleToShow, eleToHide) {
+        let currHidden = document.getElementsByClassName(eleToShow);
+        let currShow = document.getElementsByClassName(eleToHide);
+        //TODO: change so applies if multiple elements in array
+        currHidden[0].className = currHidden[0].className.replace(" hidden", "");
+        currShow[0].className += " hidden";
+    },
+    saveSettings: function () {
+        //save name
+        settings.saveName();
+        //TODO: save difficulty chose
+        //save skill point allocation
+        settings.saveSP();
+    },
+    displaySelectedSettings: function () {
+        //display name
+        settings.setName();
+        //display SP
+        settings.setSP();
     }
 }
 let view = {
     setUpEventListeners: function () {
         let buttonDiv = document.getElementById("buttonDiv");
         buttonDiv.addEventListener("click", function (event) {
-            if (event.target.className === "buttonTest") {
-                handlers.displayText();
+            if (event.target.className === "start") {
+                handlers.displayAndHide("settings", "welcome");
             }
+        });
+        let continueButton = document.getElementById("continueButton");
+        continueButton.addEventListener("click", function () {
+            handlers.displayAndHide("confirm", "settings");
+            handlers.saveSettings();
+            handlers.displaySelectedSettings();
+        });
+        let backButton = document.getElementById("backButton");
+        backButton.addEventListener("click", function () {
+            handlers.displayAndHide("settings", "confirm");
+        });
+        let confirmButton = document.getElementById("confirmButton");
+        confirmButton.addEventListener("click", function () {
+            //TODO: check for SP allocation overflow
+            handlers.displayAndHide("startgame", "confirm");
         });
     }
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
-Space Trader
-Created by:
-Danky Dank Chickens
+<h2>Space Trader</h2>
+Created by: Team 86 - Danky Dank Chickens <br/>
+<ul>
+    <li>Jeffrey Tram</li>
+    <li>Bradford Peterson</li>
+    <li>Robert Giuffreda</li>
+</ul>
