e9d71454fca280b84c12480e27df117f5aa3ee47
jeffreytram <jeffrey_tram@yahoo.com>
2019-09-27 13:36:37 -0400

    Game and Region class and TechLevel enum creation

---
+++ Space Trader/Game.py
@@ -0,0 +1,7 @@
+class Game():
+    def __init__(diff, names):
+        self.diff = diff
+        self.names = ['a','b','c','d','e','f','g','h','i','j']
+    def startGame():
+        #TODO: implement start game
+        
---
+++ Space Trader/Region.py
@@ -0,0 +1,6 @@
+class Region():
+    def __init__(x, y, techLevel, name):
+        self.x = x
+        self.y = y
+        self.techLevel = techLevel
+        self.name = name
