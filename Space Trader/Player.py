8c11d00bb245821508e925a8c5023ee41d5067dc
RobertGiuffreda <giuffredarobert@gmail.com>
2019-10-04 23:57:26 -0400

    Clean code Player

--- Space Trader/Player.py
+++ Space Trader/Player.py
@@ -1,57 +1,70 @@
+"""This is the module with the player class"""
 class Player:
-    def __init__(self, name, skillPoints, credit, currRegion):
+    """This is the player class"""
+    def __init__(self, name, skill_points, credit, curr_region):
         self.name = name
-        self.pilot = skillPoints[0]
-        self.fighter = skillPoints[1]
-        self.merchant = skillPoints[2]
-        self.engineer = skillPoints[3]
+        self.pilot = skill_points[0]
+        self.fighter = skill_points[1]
+        self.merchant = skill_points[2]
+        self.engineer = skill_points[3]
         self.credit = credit
-        self.currRegion = currRegion
+        self.curr_region = curr_region
 
     # END __init__
 
     # Temporary travel method
     def travel(self, regionTo):
+        """Moves players current region to another."""
         self.currRegion = regionTo
 
-    def getName(self):
+    def get_name(self):
+        """Return name"""
         return self.name
 
-    def getCurrRegion(self):
+    def get_curr_region(self):
+        """Returns the current region"""
         return self.currRegion
 
-    def getCredit(self):
+    def get_credit(self):
+        """Returns the players credit"""
         return self.credit
 
-    def getPilot(self):
+    def get_pilot(self):
+        """Returns the pilot skill points"""
         return self.pilot
 
-    def getFighter(self):
+    def get_fighter(self):
+        """Returns the fighter skill points."""
         return self.fighter
 
-    def getMerchant(self):
+    def get_merchant(self):
+        """Returns the merchant skillpoints"""
         return self.merchant
 
-    def getEngineer(self):
+    def get_engineer(self):
+        """Returns the engineer skill points"""
         return self.engineer
 
-    def setName(self, name):
+    def set_name(self, name):
+        """Sets the player name"""
         self.name = name
 
-    def setCurrRegion(self, currRegion):
-        self.currRegion = currRegion
-
-    def setPilot(self, pilot):
+    def set_pilot(self, pilot):
+        """Sets the pilot skill points"""
         self.pilot = pilot
 
-    def setFighter(self, fighter):
+    def set_fighter(self, fighter):
+        """Sets the fighter skill points"""
         self.fighter = fighter
 
-    def setMerchant(self, merchant):
+    def set_merchant(self, merchant):
+        """Sets the merchant skill points"""
         self.merchant = merchant
 
-    def setEngineer(self, engineer):
+    def set_engineer(self, engineer):
+        """Sets the engineer skill points"""
         self.engineer = engineer
 
-    def setCredit(self, credit):
+    def set_credit(self, credit):
+        """Sets the players skill points"""
         self.credit = credit
