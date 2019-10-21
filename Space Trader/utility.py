b6d55c297c495a75a93756b9942c58a4a3423332
RobertGiuffreda <giuffredarobert@gmail.com>
2019-10-20 21:28:56 -0400

    Put something in right place

--- Space Trader/utility.py
+++ Space Trader/utility.py
@@ -17,5 +17,5 @@ def sprice_calc(player, region):
         item.s_price = int(0.7 * (tech_factor * item.base_price * (1 + player.merchant/75)))
     if len(player.ship.cargo) != 0 or player.ship.cargo_size > 0:
         for item in player.ship.cargo:
-            tech_factor = int(0.7 * (1 - ((region.tech_level.value - item.debut)/14))
-            item.s_price = int(tech_factor * item.base_price * (1 + player.merchant/75))
+            tech_factor = (1 - ((region.tech_level.value - item.debut)/14)
+            item.s_price = int(0.7 * tech_factor * item.base_price * (1 + player.merchant/75))
