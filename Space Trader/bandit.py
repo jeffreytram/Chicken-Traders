e0a36e434b4b2c1eb4820985c188fc19c2db65aa
Brad Peterson <bpeterson48@gatech.edu>
2019-10-30 23:37:00 -0400

    Added difficulty levels per bandit name. bandit(self) spawns a new random bandit

--- Space Trader/bandit.py
+++ Space Trader/bandit.py
@@ -6,13 +6,21 @@ class Bandit:
         rand = random.randint(1,3)
         if (rand == 1):
             self.name = "Lil Peep"
+            self.demand = random.randint(10, 20)
+            self.pilot = random.randint(1, 3)
+            self.fighter = random.randint(1, 3)
         elif (rand == 2):
             self.name = "Chicky G"
+            self.demand = random.randint(20, 30)
+            self.pilot = random.randint(2, 4)
+            self.fighter = random.randint(2, 4)
         else:
             self.name = "Cluck Boss"
-        self.demand = random.randint(15, 40)
-        self.pilot = random.randint(1, 5)
-        self.fighter = random.randint(1, 5)
+            self.demand = random.randint(30, 40)
+            self.pilot = random.randint(3, 5)
+            self.fighter = random.randint(3, 5)
+        
+    
 
 
         
---
+++ Space Trader/bandittest.py
@@ -0,0 +1,10 @@
+from bandit import Bandit
+class BanditTest:
+    test = Bandit()
+    print (test.name)
+    print("Demand:")
+    print (test.demand)
+    print("Fighter:")
+    print (test.fighter)
+    print("Pilot:")
+    print (test.pilot)
