198ad75883ff95ec6e8dcabe5ce098eb7301d3f9
RobertGiuffreda <giuffredarobert@gmail.com>
2019-10-31 14:08:08 -0400

    Doing stuff

--- Space Trader/npc.py
+++ Space Trader/npc.py
@@ -1,7 +1,7 @@
 import random
 import utility
 from ship import Ship, AShip, BShip, CShip
-from item import Item, Computer, StrangeTalisman, Banana, Apple, Gun, BiggerGun, FartInAJar, Friendship, PocketRussia, InvincibilityStar, AlienChicken, AssortedAlienAnimalFurs
+from item import Item
 from universe import Universe, Region
 class NPC:
     def __init__(self, name, demand, pilot, fighter):
@@ -35,58 +35,16 @@ class Trader(NPC):
         self.name = "Trader Joe"
         self.fighter = random.randint(0, 3)
         self.merchant = random.randint(1, 5)
-        self.itemList = self.selectItems([])
+        self.itemList = self.selectItems()
     
-    def selectItems(self, list):
-        #gives the trader 3 random items, not determined by tech level b/c this happens while traveling
+    def selectItems(self):
+        #gives the trader 3 random items, not determined 
+        #by tech level b/c this happens while traveling
+        list = []
+        items = Item.__subclasses__()
         while (len(list) < 3):
-            rand = random.randint(1, 12)
-            if (rand == 1):
-                item = Computer(1)
-            elif (rand == 2):
-                item = StrangeTalisman(1)
-            elif (rand == 3):
-                item = Banana(1)
-            elif (rand == 4):
-                item = Apple(1)
-            elif (rand == 5):
-                item = Gun(1)
-            elif (rand == 6):
-                item = BiggerGun(1)
-            elif (rand == 7):
-                item = FartInAJar(1)
-            elif (rand == 8):
-                item = Friendship(1)
-            elif (rand == 9):
-                item = PocketRussia(1)
-            elif (rand == 10):
-                item = InvincibilityStar(1)
-            elif (rand == 11):
-                item = AlienChicken(1)
-            else:
-                item = AssortedAlienAnimalFurs(1)
-            #END if
-            list.append(item)
+            rand = random.randint(0, len(items) - 1)
+            list.append(item[rand](rand.rantint(1, 3)))
+            items.pop(rand)
         #END while
         return list
-
-    
-        
-        
-
-
-
-
-
-        
-    
-
-
-        
-
-
-        
-        
-
-
-    
